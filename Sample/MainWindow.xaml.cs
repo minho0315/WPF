7927e86330867d139e29b494b60c864c33389f77
hoya <62683035+minho0315@users.noreply.github.com>
2020-10-30 15:40:49 +0900

    Add files via upload
    MVVM

---
+++ Sample/MainWindow.xaml.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace Sample
+{
+    /// <summary>
+    /// MainWindow.xaml에 대한 상호 작용 논리
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+        }
+
+        private void cmdCalc_Click(object sender, RoutedEventArgs e)
+        {
+            SampleModel model = new SampleModel();
+            double d;
+            Double.TryParse(txtInput.Text, out d);
+            result.Text = string.Format("Result : {0}",model.CalculateSquareRoot(d));
+        }
+    }
+}
---
+++ SampleMvvm.Model/SampleModel.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SampleMvvm.Model
+{
+    public class SampleModel
+    {
+        public double CalculateSquareRoot(double number)
+        {
+            return Math.Sqrt(number);
+        }
+    }
+}
